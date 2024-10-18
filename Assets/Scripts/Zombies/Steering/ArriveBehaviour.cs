53adaea49b2ab0a1fe972f5224427d56a60efc44
Jakub Wiśniewski <wisniaN7@gmail.com>
2024-10-18 09:53:43 +0200

    Add `HideBehaviour`

---
+++ Assets/Scripts/Zombies/Steering/ArriveBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using DefaultNamespace;
+using UnityEngine;
+
+namespace Zombies.Steering {
+    public class ArriveBehaviour : MonoBehaviour, ISteeringBehaviour {
+        [SerializeField, Min(0f)]
+        private float _deceleration = 0.3f;
+        
+        public Vector2 target;
+        
+        public Vector2 CalculateSteering(IVehicle vehicle) {
+            Vector2 toTarget = target - (Vector2)transform.position;
+            float distance = toTarget.magnitude;
+
+            if (distance <= 0f) return Vector2.zero;
+            
+            float speed = distance / _deceleration;
+            speed = Mathf.Min(speed, vehicle.MaxSpeed);
+            Vector2 desiredVelocity = toTarget.normalized * speed / distance;
+            return desiredVelocity - vehicle.Velocity;
+        }
+    }
+}
