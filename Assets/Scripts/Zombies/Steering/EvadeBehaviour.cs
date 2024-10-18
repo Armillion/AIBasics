df1b858d12e5b7d990cf98b03dd6a38318b7f7d7
Jakub Wiśniewski <wisniaN7@gmail.com>
2024-10-18 09:53:52 +0200

    Add `EvadeBehaviour`

---
+++ Assets/Scripts/Zombies/Steering/EvadeBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using DefaultNamespace;
+using UnityEngine;
+
+namespace Zombies.Steering {
+    [RequireComponent(typeof(FleeBehaviour))]
+    public class EvadeBehaviour : MonoBehaviour, ISteeringBehaviour {
+        public IVehicle pursuer;
+
+        private FleeBehaviour _fleeBehaviour;
+
+        private void Start() {
+            _fleeBehaviour = GetComponent<FleeBehaviour>();
+        }
+
+        public Vector2 CalculateSteering(IVehicle vehicle) {
+            if (pursuer == null) return Vector2.zero;
+            
+            Vector2 toPursuer = pursuer.Position - (Vector2)transform.position;
+            float lookAheadTime = toPursuer.magnitude / (vehicle.MaxSpeed + pursuer.Velocity.magnitude);
+            
+            _fleeBehaviour.target = pursuer.Position + pursuer.Velocity * lookAheadTime;
+            return _fleeBehaviour.CalculateSteering(vehicle);
+        }
+    }
+}
